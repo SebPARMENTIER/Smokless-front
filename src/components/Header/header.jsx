881c5a6824d0fbbd6137df3205b0d10c87468997
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2022-09-27 17:51:55 +0200

    Add home and user icons and rename headersl.png into logo.png

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,11 +1,16 @@
-import logo from '../../assets/headersl.png';
+import logo from '../../assets/logo.png';
+import home from '../../assets/home.png';
+import user from '../../assets/user.png';
+
 import './header.scss';
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-image">
+        <img className="header-image-home" src={home} alt="home" />
         <img className="header-image-logo" src={logo} alt="logo" />
+        <img className="header-image-user" src={user} alt="user" />
       </div>
     </div>
   );
