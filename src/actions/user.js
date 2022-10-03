a419ea04da84277f33ab05e041681f08979e7286
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2022-10-03 16:42:51 +0200

    Update user actions

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,5 +1,22 @@
-export const SIGNUP = 'SIGNUP';
-export const createPasswordLengthError = '';
-export const createPasswordError = '';
-export const createUserSuccess = '';
-export const createUserError = '';
+export const SET_USER_VALUE = 'SET_USER_VALUE';
+export const CREATE_USER = 'CREATE_USER';
+export const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS';
+export const CREATE_USER_ERROR = 'CREATE_USER_ERROR';
+export const CREATE_PASSWORD_ERROR = 'CREATE_PASSWORD_ERROR';
+export const CREATE_PASSWORD_LENGTH_ERROR = 'CREATE_PASSWORD_LENGTH_ERROR';
+
+export const createUserValue = (newValue, name) => ({
+  type: SET_USER_VALUE,
+  value: newValue,
+  name,
+});
+
+export const createUserAction = () => ({ type: CREATE_USER });
+
+export const createUserSuccess = (data) => ({ type: CREATE_USER_SUCCESS, data });
+
+export const createUserError = () => ({ type: CREATE_USER_ERROR });
+
+export const createPasswordError = () => ({ type: CREATE_PASSWORD_ERROR });
+
+export const createPasswordLengthError = () => ({ type: CREATE_PASSWORD_LENGTH_ERROR });
