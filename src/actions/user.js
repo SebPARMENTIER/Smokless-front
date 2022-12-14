25b84d87516ae70c335a395703eafcfdd446867b
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2022-12-14 18:18:30 +0100

    Create updateAverage actions
    Create go to signup actions
    
    Update some actions

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,7 +2,8 @@ export const SET_SIGNUP_VALUE = 'SET_SIGNUP_VALUE';
 export const SIGNUP_USER = 'SIGNUP_USER';
 export const SIGNUP_USER_SUCCESS = 'SIGNUP_USER_SUCCESS';
 export const SIGNUP_USER_ERROR = 'SIGNUP_USER_ERROR';
-export const GO_BACK_TO_HOME_TO_LOGIN = 'GO_BACK_TO_HOME_TO_LOGIN';
+export const GO_TO_HOME = 'GO_TO_HOME';
+export const GO_TO_SIGNUP = 'GO_TO_SIGNUP';
 export const SET_LOGIN_VALUE = 'SET_LOGIN_VALUE';
 export const LOGIN_USER = 'LOGIN_USER';
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
@@ -18,6 +19,10 @@ export const SET_UPDATE_PASSWORD_VALUE = 'SET_UPDATE_PASSWORD_VALUE';
 export const UPDATE_PASSWORD = 'UPDATE_PASSWORD';
 export const UPDATE_PASSWORD_SUCCESS = 'UPDATE_PASSWORD_SUCCESS';
 export const UPDATE_PASSWORD_ERROR = 'UPDATE_PASSWORD_ERROR';
+export const SET_UPDATE_AVERAGE_VALUE = 'SET_UPDATE_AVERAGE_VALUE';
+export const UPDATE_AVERAGE = 'UPDATE_AVERAGE';
+export const UPDATE_AVERAGE_SUCCESS = 'UPDATE_AVERAGE_SUCCESS';
+export const UPDATE_AVERAGE_ERROR = 'UPDATE_AVERAGE_ERROR';
 export const BACK_TO_PROFILE = 'BACK_TO_PROFILE';
 
 export const signupUserValue = (newValue, name) => ({
@@ -32,7 +37,9 @@ export const signupUserSuccess = (data) => ({ type: SIGNUP_USER_SUCCESS, data })
 
 export const signupUserError = (data) => ({ type: SIGNUP_USER_ERROR, data });
 
-export const goBackToHomeToLogin = () => ({ type: GO_BACK_TO_HOME_TO_LOGIN });
+export const goToHome = () => ({ type: GO_TO_HOME });
+
+export const goToSignup = () => ({ type: GO_TO_SIGNUP });
 
 export const loginUserValue = (newValue, name) => ({
   type: SET_LOGIN_VALUE,
@@ -75,5 +82,17 @@ export const updatePasswordAction = () => ({ type: UPDATE_PASSWORD });
 export const updatePasswordSuccess = (data) => ({ type: UPDATE_PASSWORD_SUCCESS, data });
 
 export const updatePasswordError = (data) => ({ type: UPDATE_PASSWORD_ERROR, data });
+
+export const updateAverageValue = (newValue, name) => ({
+  type: SET_UPDATE_AVERAGE_VALUE,
+  value: newValue,
+  name,
+});
+
+export const updateAverageAction = () => ({ type: UPDATE_AVERAGE });
+
+export const updateAverageSuccess = (data) => ({ type: UPDATE_AVERAGE_SUCCESS, data });
+
+export const updateAverageError = (data) => ({ type: UPDATE_AVERAGE_ERROR, data });
 
 export const backToProfileAction = () => ({ type: BACK_TO_PROFILE });
